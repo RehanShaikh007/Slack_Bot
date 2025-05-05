2f14ca0bef94aa0c5f1384c7e257e5b4019a562a
Rehan Shaikh <rehanmukhtarshaikh7@gmail.com>
2025-05-05 10:31:15 +0530

    mongoDb & express setup done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+import dotenv from 'dotenv';
 
+import { App } from '@slack/bolt';
+
+import express from 'express';
+
+import mongoose from 'mongoose';
+
+// Initialize Slack app
+const app = new App({
+    token: process.env.SIGNING_SECRET_KEY,
+    socketMode: true,
+})
+
+// Initialize Express app
+const expressApp = express();
+expressApp.use(express.json());
+
+// Initialize MongoDB connection
+mongoose.connect(process.env.MONGO_URI) 
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
+
+    
